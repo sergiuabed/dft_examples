43cbad9bd29d5fa5558bb23b2c11358c4ededa25
divadnauj-GB <juan.david.guerrero.balaguera@gmail..com>
2026-03-27 01:04:26 -0400

    Nvdla DfT example EDT

---
+++ 6.nvdla_edt_gate/NV_NVDLA_partition_a/7.generate_atpg_patterns/1.run_graybox_generation.sh
@@ -0,0 +1,58 @@
+#\! /bin/sh -f
+#\
+mkdir -p logfiles
+#\
+mkdir -p patterns
+#\
+exec tessent -shell -log logfiles/$0.log -replace -dofile "$0"
+
+set PDK_ROOT ../../../tech
+
+# Specify the same design ID that is used during scan insertion
+set_context patterns -scan -design_id gate3
+
+# Specify where the tsdb_outdir is to be located, default is at the current working directory
+set_tsdb_output_directory ../tsdb_outdir
+
+# Read Tessent Library
+read_cell_library ${PDK_ROOT}/tessent/ihp-sg13g2_stdcell.tcelllib
+read_cell_library ${PDK_ROOT}/tessent/ihp-sg13g2_io.tcelllib
+
+#read_cell_library ${PDK_ROOT}/tessent/memories/RM_IHPSG13_1P_256x64_c2_bm_bist.atpglib
+
+# read_verilog ../../nv_small/outdir/nv_small/vmod/rams/model/RAMDP_16X272_GL_M1_E2.v -verbose -exclude_from_file_dictionary -interface_only
+# read_verilog ../../nv_small/outdir/nv_small/vmod/rams/model/RAMDP_16X256_GL_M1_E2.v -verbose -exclude_from_file_dictionary -interface_only
+
+# Read the scan inserted design
+read_design NV_NVDLA_partition_a  -design_id gate3 -verbose
+
+set_current_design NV_NVDLA_partition_a
+
+report_black_boxes -undefined
+add_black_boxes -modules {\
+                        RAMDP_16X272_GL_M1_E2 \
+                        RAMDP_16X256_GL_M1_E2 \
+                        } 
+
+
+report_dft_signals
+
+# DRC check the scan chains in each mode and create graybox for external mode
+foreach_in_collection mode_wrapper [get_config_elements Core(NV_NVDLA_partition_a)/Scan/Mode -part tcd -silent] {
+  set mode_name [get_config_value $mode_wrapper -id <0>]
+  set mode_type [get_config_value type -in $mode_wrapper]
+  import_scan_mode $mode_name
+  check_design_rules
+  if {$mode_type eq "external"} {
+    report_scan_cells
+    analyze_graybox
+    write_design -tsdb -graybox -verbose
+    write_core_description ./NV_NVDLA_partition_a_gray_box.tcd -replace
+  }
+  set_system_mode setup
+
+}
+
+exit
+
+
---
+++ 6.nvdla_edt_gate/NV_NVDLA_partition_c/7.generate_atpg_patterns/1.run_graybox_generation.sh
@@ -0,0 +1,55 @@
+#\! /bin/sh -f
+#\
+mkdir -p logfiles
+#\
+mkdir -p patterns
+#\
+exec tessent -shell -log logfiles/$0.log -replace -dofile "$0"
+
+set PDK_ROOT ../../../tech
+
+# Specify the same design ID that is used during scan insertion
+set_context patterns -scan -design_id gate3
+
+# Specify where the tsdb_outdir is to be located, default is at the current working directory
+set_tsdb_output_directory ../tsdb_outdir
+
+# Read Tessent Library
+read_cell_library ${PDK_ROOT}/tessent/ihp-sg13g2_stdcell.tcelllib
+read_cell_library ${PDK_ROOT}/tessent/ihp-sg13g2_io.tcelllib
+
+#read_cell_library ${PDK_ROOT}/tessent/memories/RM_IHPSG13_1P_256x64_c2_bm_bist.atpglib
+
+# Read the scan inserted design
+read_design NV_NVDLA_partition_c  -design_id gate3 -verbose
+set_current_design NV_NVDLA_partition_c
+
+report_black_boxes -undefined
+add_black_boxes -modules {\
+                        RAMPDP_256X64_GL_M2_D2 \
+                        RAMDP_16X64_GL_M1_E2 \
+                        RAMDP_8X66_GL_M1_E2 \
+                        RAMDP_128X6_GL_M2_E2 \
+                        RAMDP_128X11_GL_M2_E2 \
+                        } 
+report_dft_signals
+
+# DRC check the scan chains in each mode and create graybox for external mode
+foreach_in_collection mode_wrapper [get_config_elements Core(NV_NVDLA_partition_c)/Scan/Mode -part tcd -silent] {
+  set mode_name [get_config_value $mode_wrapper -id <0>]
+  set mode_type [get_config_value type -in $mode_wrapper]
+  import_scan_mode $mode_name
+  check_design_rules
+  if {$mode_type eq "external"} {
+    report_scan_cells
+    analyze_graybox
+    write_design -tsdb -graybox -verbose
+    write_core_description ./NV_NVDLA_partition_c_gray_box.tcd -replace
+  }
+  set_system_mode setup
+
+}
+
+exit
+
+
